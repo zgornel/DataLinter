92878d98e4286ab1435e518dfcfb015d54c65d22
Corneliu Cofaru <cornel@oxoaresearch.com>
2024-08-19 17:20:42 +0200

    Add simple script

---
+++ VUBLinter.jl/scripts/simple_run.jl
@@ -0,0 +1,27 @@
+#using Revise
+using Pkg
+Pkg.activate(joinpath(dirname(@__FILE__),".."))  # we assume that this file lies in ./scripts
+
+using VUBLinter
+
+data = Vector{Vector{Union{Missing, Float64}}}([rand(3) for _ in 1:5])
+
+# Alter data to produce linting output
+data[1][1] = -1
+data[5][2] = missing
+
+code = "apply_some_classifier"  # some sample code, will activate only the missing rule
+
+kbpath = expanduser("~/vub/code/vublinter/VUBLinter.jl/knowledge/linting.toml")
+kb = VUBLinter.kb_load(kbpath)
+
+# First case, print to stdout linting on data
+buf =stdout
+ctx_no_code = VUBLinter.build_data_context(data; elementwise=false)
+lintout = VUBLinter.lint(ctx_no_code, kb, buffer=buf, show_stats=true, show_passing=false);
+
+# Second case, print to buffer (and print the buffer), linting on data+code
+buf = IOBuffer();
+ctx_code = VUBLinter.build_data_context(data, code; elementwise=false)
+lintout = VUBLinter.lint(ctx_code, kb, buffer=buf, show_stats=true, show_passing=false);
+VUBLinter.OutputInterface.print_buffer(buf)
