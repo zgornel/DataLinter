04c5a053b9db5d762881af4189fce5ad361ea2ba
Documenter.jl <documenter@juliadocs.github.io>
2024-09-04 17:52:45 +0000

    build based on 40125cb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"api/#API-Documentation","page":"API Reference","title":"API Documentation","text":"","category":"section"},{"location":"examples/#Usage-examples","page":"Usage examples","title":"Usage examples","text":"","category":"section"},{"location":"#VUB-Data-linter-documentation","page":"Introduction","title":"VUB Data linter documentation","text":"","category":"section"}]
+}
