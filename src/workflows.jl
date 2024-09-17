0dee307a43c973c5e8e632a61eab255556cb98eb
Corneliu Cofaru <cornel@oxoaresearch.com>
2024-09-17 15:40:02 +0200

    Update cli linting workflow with kwargs

--- src/workflows.jl
+++ src/workflows.jl
@@ -1,6 +1,10 @@
-function cli_linting_workflow(filepath, kbpath, args...)
+function cli_linting_workflow(filepath,
+                              kbpath;
+                              buffer=stdout,
+                              show_stats=true,
+                              show_passing=false,
+                              show_na=false)
     kb = VUBLinter.kb_load(kbpath)
-    buf =stdout
     ctx = VUBLinter.DataInterface.build_data_context(filepath)
-    lintout = lint(ctx, kb, buffer=buf, show_stats=true, show_passing=false, show_na=false);
+    lintout = lint(ctx, kb; buffer, show_stats, show_passing, show_na);
 end
