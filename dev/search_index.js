d25fc62f814dd10a94a73ba793d1c712667e907a
Documenter.jl <documenter@juliadocs.github.io>
2024-10-17 14:14:20 +0000

    build based on bde4c70

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"api/#API-Documentation","page":"API Reference","title":"API Documentation","text":"","category":"section"},{"location":"examples/#Usage-examples","page":"Usage examples","title":"Usage examples","text":"","category":"section"},{"location":"#VUB-Data-linter-documentation","page":"Introduction","title":"VUB Data linter documentation","text":"","category":"section"}]
+[{"location":"api/#API-Documentation","page":"API Reference","title":"API Documentation","text":"","category":"section"},{"location":"examples/#Usage-examples","page":"Usage examples","title":"Usage examples","text":"","category":"section"},{"location":"#DataLinter-documentation","page":"Introduction","title":"DataLinter documentation","text":"","category":"section"}]
 }
