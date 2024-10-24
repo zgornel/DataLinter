6bf4aff38ce947908b37d4e09dff88c2bd52f4b0
Corneliu Cofaru <cornel@oxoaresearch.com>
2024-10-24 18:29:30 +0200

    Add config/default.toml; bugfix in app precompilation

--- apps/datalinter/precompile.jl
+++ apps/datalinter/precompile.jl
@@ -3,9 +3,10 @@ using DataLinter
 
 # Use <PROJECT_ROOT>/test/data/data.csv
 datapath = joinpath(dirname(@__FILE__), "..", "..", "test", "data", "data.csv")
+configpath = joinpath(dirname(@__FILE__), "..", "..", "config", "default.toml")
 kb = DataLinter.kb_load("")
 data =DataLinter._generate_workload_data(1000)
 ctx = DataLinter.build_data_context(data);
 DataLinter.lint(ctx, kb; buffer=IOBuffer(), show_passing=false);
-DataLinter.cli_linting_workflow(datapath, ""; buffer=IOBuffer())
+DataLinter.cli_linting_workflow(datapath, "", configpath; buffer=IOBuffer())
 #datalinter.julia_main()
