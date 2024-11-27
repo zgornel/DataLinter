f21cf5b0cc0a2d8fb06f971f7b80376be8262dc9
Corneliu Cofaru <cornel@oxoaresearch.com>
2024-11-27 18:24:05 +0100

    Bugfix for datalinter cli app precompilation

--- apps/datalinter/precompile.jl
+++ apps/datalinter/precompile.jl
@@ -7,6 +7,21 @@ configpath = joinpath(dirname(@__FILE__), "..", "..", "config", "default.toml")
 kb = DataLinter.kb_load("")
 data =DataLinter._generate_workload_data(1000)
 ctx = DataLinter.build_data_context(data);
-DataLinter.lint(ctx, kb; buffer=IOBuffer(), show_passing=false);
-DataLinter.cli_linting_workflow(datapath, "", configpath; buffer=IOBuffer())
+DataLinter.lint(ctx, kb; debug=true);
+DataLinter.cli_linting_workflow(datapath,
+                                "",
+                                configpath;
+                                buffer=IOBuffer(),
+                                show_stats=true,
+                                show_passing=false,
+                                show_na=false,
+                                progress=false)
+DataLinter.cli_linting_workflow(datapath,
+                                "",
+                                configpath;
+                                buffer=IOBuffer(),
+                                show_stats=true,
+                                show_passing=true,
+                                show_na=true,
+                                progress=true)
 #datalinter.julia_main()
