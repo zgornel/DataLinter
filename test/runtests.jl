719e2169fe4a93225b725583b5d7d5c0c109bae0
Corneliu Cofaru <cornel@oxoaresearch.com>
2025-01-07 11:43:15 +0100

    Squashed commit for tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,7 @@ using DataLinter
 using Logging
 global_logger(ConsoleLogger(stdout, Logging.Error))  # supress test warnings
 
-#TODO: Implement tests for the other modules of DataLinter
 include("config.jl")
+include("data.jl")
+include("linter.jl")
+include("kb.jl")
