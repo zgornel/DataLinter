7c641cfcc72740d967317e84f67c30929260a5d2
Corneliu Cofaru <cornel@oxoaresearch.com>
2026-02-17 18:01:49 +0100

    Excised ipython stuff; Fixes #43

--- ipython/datalintermagics/__init__.py
+++
@@ -1,9 +0,0 @@
-"""IPython magic for DataLinter"""
-__version__ = '0.0.1'
-
-
-from .lintermagic import DataLinterMagic
-
-
-def load_ipython_extension(ipython):
-    ipython.register_magics(DataLinterMagic)
