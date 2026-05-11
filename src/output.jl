8cd61a4fdada0cd5b3d5fef00f52508d68d42b51
Corneliu Cofaru <cornel@oxoaresearch.com>
2026-05-11 23:40:53 +0200

    Bugfix in linting stats

--- src/output.jl
+++ src/output.jl
@@ -48,17 +48,19 @@ function process_output(
         show_passing = false,
         show_na = false
     )
-    n_failures = 0
     n_linters = map(lo -> lo[1][1].name, lintout) |> length ∘ unique
     n_linters_applied = map(lo -> lo[1][1].name, filter(lo -> !isa(lo[2], NotAvailableCheck), lintout)) |> length ∘ unique
     n_linters_na = n_linters - n_linters_applied
     sorted_out = sort(lintout, by = l -> get(WARN_LEVEL_TO_NUM, (l[1][1]).warn_level, 0), rev = true)
     give_reason_for_na(result) = result.info === nothing ? "*not applicable*" : "*FAILED*"
+    failed_linters = Symbol[]
     for ((linter, loc_name), result) in sorted_out
         msg, color, bold = _print_options(result, linter)
         if !(result isa NotAvailableCheck)
             if result isa FailedCheck  # linter failed
-                n_failures += 1
+                if linter.name ∉ failed_linters
+                    push!(failed_linters, linter.name)
+                end
                 printstyled(buffer, "$(rpad("$msg", 15))\t$(rpad("($(linter.name))", 30))\t"; color, bold)
                 printstyled(buffer, "$(rpad(loc_name, 20)) "; color, bold)
                 printstyled(buffer, "$(linter.failure_message(loc_name, result))\n"; color, bold)
@@ -76,6 +78,7 @@ function process_output(
         end
     end
     if show_stats
+        n_failures = length(failed_linters)
         printstyled(buffer, "Total of $n_linters linters:")
         printstyled(buffer, " $(n_linters - n_failures - n_linters_na) Pass", bold=true, color = :green)
         printstyled(buffer, ",")
