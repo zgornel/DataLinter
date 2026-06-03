f99b2941b844ad875ea62334f7ce65aca9b6003b
Corneliu Cofaru <cornel@oxoaresearch.com>
2026-06-03 13:16:36 +0200

    Update data-only script

--- scripts/data_only_run.jl
+++ scripts/data_only_run.jl
@@ -6,27 +6,31 @@ using Dates
 using Tables
 using DataLinter
 
-n = 10_000
-data = Vector{Any}(Vector{Union{Missing, Float64}}[rand(n) for _ in 1:3])
-push!(data, Vector{Union{Missing, Float64}}(ones(n) .* 1000))
-push!(data, [string.(Date(Dates.now())) for _ in 1:n])
-push!(data, [(randstring(3) * " " * randstring(2)) for _ in 1:n])
-push!(data, [string(rand()) for _ in 1:n])
-# Alter data to produce linting output
-data[1][1] = -10
-data[3][2] = missing
-push!(data, rand(["a", "b", ["a", "b"]], n))
-empty_row = 10
-for col in data
-    col[empty_row] = ifelse(typeof(col[empty_row]) <: Number, missing, "")
-end
-duplicates = (10 => 11, 12 => 13)
-for (srow, drow) in duplicates
+function _generate_workload_data(n = 100)
+    data = Vector{Any}(Vector{Union{Missing, Float64}}[rand(n) for _ in 1:3])
+    push!(data, Vector{Union{Missing, Float64}}(ones(n) .* 1000))
+    push!(data, [string.(Date(Dates.now())) for _ in 1:n])
+    push!(data, [(randstring(3) * " " * randstring(2)) for _ in 1:n])
+    push!(data, [string(rand()) for _ in 1:n])
+    # Alter data to produce linting output
+    data[1][1] = -10
+    data[3][2] = missing
+    push!(data, rand(["a", "b", ["a", "b"]], n))
+    empty_row = 10
     for col in data
-        col[drow] = col[srow]
+        col[empty_row] = ifelse(typeof(col[empty_row]) <: Number, missing, "")
     end
+    duplicates = (10 => 11, 12 => 13)
+    for (srow, drow) in duplicates
+        for col in data
+            col[drow] = col[srow]
+        end
+    end
+    return Tables.Columns(Dict(Symbol("x$i") => data[i] for i in 1:length(data)))
 end
 
+
+data = _generate_workload_data(1000) 
 kbpath = joinpath(dirname(@__FILE__), "..", "knowledge", "linting.toml")
 kb = DataLinter.kb_load(kbpath)
 
